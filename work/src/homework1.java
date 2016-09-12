9f4e41491c0509901cc8cfe2106c0245d986421f
Prin0516 <105021046@live.asia.edu.tw>
2016-09-12 15:45:54 +0800

    hm.00

---
+++ work/src/homework1.java
@@ -0,0 +1,12 @@
+
+public class homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我是賴沛錞\n" +
+				"我的興趣是看電視\n" +
+				"就讀亞洲大學");
+
+	}
+
+}
